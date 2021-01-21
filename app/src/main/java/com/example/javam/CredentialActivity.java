77a1b9d58d043f6ceb4348faf39a1884bac39196
DaveDarko <a17100279@ceti.mx>
2021-01-20 23:49:33 -0600

    Primer funcionamiento del Proyecto

---
+++ app/src/main/java/com/example/javam/CredentialActivity.java
@@ -0,0 +1,44 @@
+package com.example.javam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.javam.utils.InputValidation;
+
+public class CredentialActivity extends AppCompatActivity {
+
+    private EditText editTextEmail, editTextPassword;
+    private Button buttonCompose;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credential);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonCompose = findViewById(R.id.buttonCompose);
+
+        buttonCompose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (InputValidation.isValidEditText(editTextEmail, getString(R.string.field_is_required))
+                        && InputValidation.isValidEditText(editTextPassword, getString(R.string.field_is_required))) {
+                    redirectToMainActivity();
+                }
+            }
+        });
+    }
+
+    private void redirectToMainActivity() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.putExtra("recipientEmail", editTextEmail.getText().toString());
+        intent.putExtra("recipientPassword", editTextPassword.getText().toString());
+        startActivity(intent);
+    }
+}
